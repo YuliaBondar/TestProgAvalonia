6e1b4da870e93b760e2f6ae0a419785f624bc85d
Бондарь Юлия Владимировна <bondar_yuv@peleng.jsc.local>
2025-07-01 16:14:41 +0300

    add data for weather

---
+++ Avalonia.Models/WeatherData.cs
@@ -0,0 +1,12 @@
+﻿namespace Avalonia.Models
+{
+    public class WeatherData
+    {
+        public required string CountryCityName { get; set; }
+        public double Temperature { get; set; }
+        public required string Description {  get; set; }
+        public DateTime DateTime { get; set; }
+        public required string Country { get; set; }
+
+    }
+}
--- TestViewModel/WeatherAvalonia.cs
+++ TestViewModel/WeatherAvalonia.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reactive.Linq;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
 
 namespace Avalonia.ViewModel
 {
-    internal class MainWindowAvalonia
+    internal class WeatherAvalonia : ReactiveObject
     {
+       
+        
     }
 }
